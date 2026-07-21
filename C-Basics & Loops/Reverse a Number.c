8a48507b85d848003206dba1c26f9a83f14f29c2
Praveenkotha-occ <prawinkotha@gmail.com>
2026-07-22 01:46:11 +0530

    Add files via upload

---
+++ C-Basics & Loops/Reverse a Number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n ;
+    scanf("%d",&n);
+    int rev=0;
+    while(n!=0){
+        int digit=n%10;
+        rev=rev*10+digit;
+        n/=10;
+    }
+    printf("%d",rev);
+}
